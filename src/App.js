ea30bd628ba9cfcae89bca741cf07457effe65f9
Francis Bello <bellofrancis87@gmail.com>
2023-05-29 20:50:38 +0100

    loader still

--- src/App.js
+++ src/App.js
@@ -16,29 +16,25 @@ function App() {
     };
   }, []);
 
-  return (
+  return !loading ? (
     <div>
-      {loading ? (
-        <div className="w-full h-screen flex items-center justify-center">
-          <div className="wavy sm:text-3xl text-xl font-semibold">
-            <span style={{ "--i": 1 }}>F</span>
-            <span style={{ "--i": 2 }}>b</span>
-            <span style={{ "--i": 3 }}>.</span>
-            <span style={{ "--i": 4 }}>.</span>
-            <span style={{ "--i": 5 }}>.</span>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <Navbar />
-          <Home />
-          <About />
-          <Tech />
-          <Project />
-          <Contact />
-          <Footer />
-        </div>
-      )}
+      <Navbar />
+      <Home />
+      <About />
+      <Tech />
+      <Project />
+      <Contact />
+      <Footer />
+    </div>
+  ) : (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="wavy sm:text-3xl text-xl font-semibold">
+        <span style={{ "--i": 1 }}>F</span>
+        <span style={{ "--i": 2 }}>b</span>
+        <span style={{ "--i": 3 }}>.</span>
+        <span style={{ "--i": 4 }}>.</span>
+        <span style={{ "--i": 5 }}>.</span>
+      </div>
     </div>
   );
 }
