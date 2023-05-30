05e79388cdb091b698146d9d24612073d2e546c1
Francis Bello <bellofrancis87@gmail.com>
2023-05-30 16:28:05 +0100

    finally

--- src/App.js
+++ src/App.js
@@ -3,32 +3,32 @@ import { About, Contact, Footer, Home, Project, Tech } from "./containers";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const handleLoad = () => {
-      setLoading(false);
-    };
-    window.addEventListener("load", handleLoad);
+  // useEffect(() => {
+  //   const handleLoad = () => {
+  //     setLoading(false);
+  //   };
+  //   window.addEventListener("load", handleLoad);
 
-    return () => {
-      window.removeEventListener("load", handleLoad);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("load", handleLoad);
+  //   };
+  // }, []);
 
-  if (loading) {
-    return (
-      <div className="w-full h-screen flex items-center justify-center">
-        <div className="wavy sm:text-3xl text-xl font-semibold">
-          <span style={{ "--i": 1 }}>F</span>
-          <span style={{ "--i": 2 }}>b</span>
-          <span style={{ "--i": 3 }}>.</span>
-          <span style={{ "--i": 4 }}>.</span>
-          <span style={{ "--i": 5 }}>.</span>
-        </div>
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div className="w-full h-screen flex items-center justify-center">
+  //       <div className="wavy sm:text-3xl text-xl font-semibold">
+  //         <span style={{ "--i": 1 }}>F</span>
+  //         <span style={{ "--i": 2 }}>b</span>
+  //         <span style={{ "--i": 3 }}>.</span>
+  //         <span style={{ "--i": 4 }}>.</span>
+  //         <span style={{ "--i": 5 }}>.</span>
+  //       </div>
+  //     </div>
+  //   );
+  // }
   return (
     <div>
       <Navbar />
